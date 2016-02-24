cf7cb8068edb7e94585e99e48dc85c3e7e0dc9d4
CHJ <842745156@qq.com>
2016-02-25 00:43:21 +0800

    commit before go to sleep

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 // head获取时间
 var mydate=new Date();
 var year=mydate.getFullYear();
-var month=mydate.getMonth();
+var monthnum=mydate.getMonth();
+var month=["January","Feberary","March","April","May","June","July","August","September","October","November","December"];
 var date=mydate.getDate();
-document.getElementById("getdate").innerHTML=year+"."+month+"."+date;
+document.getElementById("getdate").innerHTML=month[monthnum]+"."+date+"."+year;
+
+//购物车数据
+var itemnum=0;
+var totalprice=0.00;
+document.getElementById("itemnum").firstChild.innerHTML=itemnum;
+document.getElementById("totalprice").innerHTML="$"+totalprice;
